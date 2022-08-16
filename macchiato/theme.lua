d886245ee465d08ed4770a04623d790fd09f0ae0
AdalZanabria <adal.zanabria@gmail.com>
2022-08-16 18:26:33 -0500

    Added the macchiato v0.2.0 flavour.

---
+++ macchiato/theme.lua
@@ -0,0 +1,42 @@
+-- Catppuccin Macchiato Theme for tym
+
+local rosewater = '#F4DBD6'
+local red = '#ED8796'
+local green = '#A6DA95'
+local yellow = '#EED49F'
+local blue = '#8AADF4'
+local pink = '#F5BDE6'
+local teal = '#8BD5CA'
+local text = '#CAD3F5'
+local subtext1 = '#B8C0E0'
+local subtext0 = '#A5ADCB'
+local surface2 = '#5B6078'
+local surface1 = '#494D64'
+local base = '#24273A'
+
+return {
+  color_background = base,
+  color_foreground = text,
+  color_window_background = base,
+  color_bold = text,
+  color_cursor = rosewater,
+  color_cursor_foreground = base,
+  color_highlight = rosewater,
+  color_highlight_foreground = base,
+  color_0  = surface1, -- black (background)
+  color_1  = red, -- red
+  color_2  = green, -- green
+  color_3  = yellow, -- brown
+  color_4  = blue, -- blue
+  color_5  = pink, -- purple
+  color_6  = teal, -- cyan
+  color_7  = subtext1, -- light gray (foreground)
+  color_8  = surface2, -- gray
+  color_9  = red, -- light red
+  color_10 = green, -- light green
+  color_11 = yellow, -- yellow
+  color_12 = blue, -- light blue
+  color_13 = pink, -- pink
+  color_14 = teal, -- light cyan
+  color_15 = subtext0, -- white
+}
