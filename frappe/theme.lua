8a0cffe10b3d657fe6620b9fbc78d646a02f843f
AdalZanabria <adal.zanabria@gmail.com>
2022-08-16 18:03:28 -0500

    Added the frappe v0.2.0 palette and added the color_window_background property to latte.

--- frappe/theme.lua
+++ frappe/theme.lua
@@ -1,18 +1,18 @@
--- Catppuccin Latte Theme for tym
+-- Catppuccin Frappe Theme for tym
 
-local rosewater = '#DC8A78'
-local red = '#D20F39'
-local green = '#40A02B'
-local yellow = '#DF8E1D'
-local blue = '#1E66F5'
-local pink = '#EA76CB'
-local teal = '#179299'
-local text = '#4C4F69'
-local subtext1 = '#5C5F77'
-local subtext0 = '#6C6F85'
-local surface2 = '#ACB0BE'
-local surface1 = '#BCC0CC'
-local base = '#EFF1F5'
+local rosewater = '#F2D5CF'
+local red = '#E78284'
+local green = '#A6D189'
+local yellow = '#E5C890'
+local blue = '#8CAAEE'
+local pink = '#F4B8E4'
+local teal = '#81C8BE'
+local text = '#C6D0F5'
+local subtext1 = '#B5BFE2'
+local subtext0 = '#A5ADCE'
+local surface2 = '#626880'
+local surface1 = '#51576D'
+local base = '#303446'
 
 return {
   color_background = base,
@@ -23,20 +23,20 @@ return {
   color_cursor_foreground = base,
   color_highlight = rosewater,
   color_highlight_foreground = base,
-  color_0  = subtext1, -- black (background)
+  color_0  = surface1, -- black (background)
   color_1  = red, -- red
   color_2  = green, -- green
   color_3  = yellow, -- brown
   color_4  = blue, -- blue
   color_5  = pink, -- purple
   color_6  = teal, -- cyan
-  color_7  = surface2, -- light gray (foreground)
-  color_8  = subtext0, -- gray
+  color_7  = subtext1, -- light gray (foreground)
+  color_8  = surface2, -- gray
   color_9  = red, -- light red
   color_10 = green, -- light green
   color_11 = yellow, -- yellow
   color_12 = blue, -- light blue
   color_13 = pink, -- pink
   color_14 = teal, -- light cyan
-  color_15 = surface1, -- white
+  color_15 = subtext0, -- white
 }
