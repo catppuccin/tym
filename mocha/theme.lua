40ae5c16ee850c562de26ff373c593060e07b256
AdalZanabria <adal.zanabria@gmail.com>
2022-08-16 22:27:04 -0500

    Added mocha v0.2.0 flavour and screenshots, deleted deprecated screenshot.

---
+++ mocha/theme.lua
@@ -0,0 +1,42 @@
+-- Catppuccin Mocha Theme for tym
+
+local rosewater = '#F5E0DC'
+local red = '#F38BA8'
+local green = '#A6E3A1'
+local yellow = '#F9E2AF'
+local blue = '#89B4FA'
+local pink = '#F5C2E7'
+local teal = '#94E2D5'
+local text = '#CDD6F4'
+local subtext1 = '#BAC2DE'
+local subtext0 = '#A6ADC8'
+local surface2 = '#585B70'
+local surface1 = '#45475A'
+local base = '#1E1E2E'
+
+return {
+  color_background = base,
+  color_foreground = text,
+  color_window_background = base,
+  color_bold = text,
+  color_cursor = rosewater,
+  color_cursor_foreground = base,
+  color_highlight = rosewater,
+  color_highlight_foreground = base,
+  color_0  = surface1, -- black (background)
+  color_1  = red, -- red
+  color_2  = green, -- green
+  color_3  = yellow, -- brown
+  color_4  = blue, -- blue
+  color_5  = pink, -- purple
+  color_6  = teal, -- cyan
+  color_7  = subtext1, -- light gray (foreground)
+  color_8  = surface2, -- gray
+  color_9  = red, -- light red
+  color_10 = green, -- light green
+  color_11 = yellow, -- yellow
+  color_12 = blue, -- light blue
+  color_13 = pink, -- pink
+  color_14 = teal, -- light cyan
+  color_15 = subtext0, -- white
+}
